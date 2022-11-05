75313a0ac7d5659ed5177b7de95f4c2ea2b065d2
Jaehwan <dialektike@gmail.com>
2022-11-05 23:47:11 +0900

    main.rs 추가

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use library::pick;
+use library::postfix;
+use library::verifiers;
+
+fn main() {
+    // 테스트
+    println!("결과: {}", postfix("테스트", "은"));
+    println!("결과: {}.", pick("테스트", "은"));
+    println!("결과: {:?}", verifiers("apple", "is"));
+}
