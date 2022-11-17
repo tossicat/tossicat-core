7cfe04ab24ebfe88f0950fa1704c3cc54e9a09ff
Jaehwan <dialektike@gmail.com>
2022-11-17 12:57:26 +0900

    테스트 함수 이름 변경 및 내용 추가

--- tests/eul_ka_ida_neun.rs
+++ tests/eul_ka_ida_neun.rs
@@ -1,7 +1,7 @@
 use library::{look_up_in_eul_ka_ida_neun, EUL};
 
 #[test]
-fn _chang_with_final_letter() {
+fn _look_up_in_eul_ka_ida_neun() {
     let temp = "네이버";
     let result = "를";
     assert_eq!(result, look_up_in_eul_ka_ida_neun(temp, EUL));
@@ -11,6 +11,7 @@ fn _chang_with_final_letter() {
     assert_eq!(result, look_up_in_eul_ka_ida_neun(temp, EUL));
     // 괄호 안에 들어 있는 글자는 무시하고 바로 앞 글자가 마지막 글자가 됩니다.
     let temp = "넥슨(코리아)";
+    let result = "을";
     assert_eq!(result, look_up_in_eul_ka_ida_neun(temp, EUL));
     // 숫자는 그 숫자를 한글로 발음하는 것으로 변환합니다.
     let temp = "비타500";
