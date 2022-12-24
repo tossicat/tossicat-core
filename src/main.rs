7339ee70bda0b2825530bd968290e409691b6911
Jaehwan <dialektike@gmail.com>
2022-12-24 18:58:10 +0900

    README.md case 실행 코드 추가 및 오타 수정

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,14 @@ use tossicat::verifiers;
 
 fn main() {
     // 테스트
+    println!("결과: {}", postfix("사과", "을"));
+    println!("결과: {}", pick("사과", "을"));
     println!("결과: {}", postfix("테스트", "처럼"));
-    println!("결과: {}.", pick("테스트", "은"));
-    println!("결과: {}.", pick("구글", "으로부터"));
-    println!("결과: {}.", pick("토시캣", "보다"));
-    println!("결과: {}.", pick("네이버", "에서부터"));
+    println!("결과: {}", postfix("천사1004", "은"));
+    println!("결과: {}", pick("테스트", "은"));
+    println!("결과: {}", pick("구글", "으로부터"));
+    println!("결과: {}", pick("토시캣", "보다"));
+    println!("결과: {}", pick("네이버", "에서부터"));
     println!("결과: {:?}", verifiers("apple", "은"));
     println!("결과: {:?}", verifiers("apple", "는"));
     println!("결과: {:?}", verifiers("apple", "is"));
