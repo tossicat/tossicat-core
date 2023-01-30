4910f8334151f0445c20f61263b2e431128a1f32
Jaehwan <dialektike@gmail.com>
2023-01-30 12:17:30 +0900

    pick() 함수를 제거 합니다.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,17 @@
 use tossicat::join_phonemes;
 use tossicat::modify_sentence;
-use tossicat::pick;
 use tossicat::postfix;
 
 fn main() {
     // 테스트
     println!("결과: {:?}", postfix("사과", "을"));
     println!("결과: {:?}", postfix("apple", "을"));
-    println!("결과: {:?}", pick("사과", "을"));
     println!("결과: {:?}", postfix("테스트", "처럼"));
     println!("결과: {:?}", postfix("천사1004", "은"));
-    println!("결과: {:?}", pick("테스트", "류현지"));
-    println!("결과: {:?}", pick("구글", "으로부터"));
-    println!("결과: {:?}", pick("토시캣", "보다"));
-    println!("결과: {:?}", pick("네이버", "코코"));
+    println!("결과: {:?}", postfix("테스트", "류현지"));
+    println!("결과: {:?}", postfix("구글", "으로부터"));
+    println!("결과: {:?}", postfix("토시캣", "보다"));
+    println!("결과: {:?}", postfix("네이버", "코코"));
     println!("결과: {:?}", postfix("누구", "이"));
     println!("결과: {:?}", join_phonemes(['ㅋ', 'ㅔ', 'ㅅ']));
     let test = "{철수, 은} {영희,   과} {밥,  를} 먹습니다.";
